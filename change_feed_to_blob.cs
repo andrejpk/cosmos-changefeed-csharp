362b7662290e7c7c68bda150e0aa30bd42dbfc76
Andrej Kyselica <andrej@kyselica.com>
2019-03-21 09:49:21 -0400

    prelim version, need to fix bindings

--- change_feed_to_blob.cs
+++ change_feed_to_blob.cs
@@ -1,24 +1,32 @@
 using System.Collections.Generic;
 using Microsoft.Azure.Documents;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace cosmos_changefeed_csharp
 {
     public static class change_feed_to_blob
     {
-        [FunctionName("change_feed_to_blob")]
+        [FunctionName("change_feed_to_blob")]    
+        [StorageAccount("storageData")]
         public static void Run([CosmosDBTrigger(
-            databaseName: "databaseName",
-            collectionName: "collectionName",
-            ConnectionStringSetting = "",
-            LeaseCollectionName = "leases")]IReadOnlyList<Document> input, ILogger log)
+            databaseName: "SensorData",
+            collectionName: "Telemetry",
+            ConnectionStringSetting = "cosmosdb",
+            CreateLeaseCollectionIfNotExists = true,
+            LeaseCollectionName = "leasescsharp")]IReadOnlyList<Document> input,
+            [Blob("outputcsharp/{rand-guid}.json", FileAccess.Write)] Stream outBlob,
+            ILogger log)
         {
             if (input != null && input.Count > 0)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+                using (StreamWriter sw = new StreamWriter(outBlob)) {
+                    sw.Write(input.ToString());
+                }
             }
         }
     }
